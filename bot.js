d717eeb7816cfb9ba2c525e169109afc6f73d524
yoni chanowitz <yonichanowitz@gmail.com>
2018-07-30 17:12:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ logger.info(‘Logged in as: ‘);
 logger.info(bot.username + ‘ – (‘ + bot.id + ‘)’);
 });
 bot.on(‘message’, function (user, userID, channelID, message, evt) {
-if (message.substring(0, 1) == ‘!’) {
+if (message.substring(0, 1) == ‘!rtb’) {
 var args = message.substring(1).split(‘ ‘);
 var cmd = args[0];
 
@@ -26,8 +26,13 @@ switch(cmd) {
 case ‘intro’:
 bot.sendMessage({
 to: channelID,
-message: "Greetings! Welcome to the server!"
+message: "what do you want from me?"
 });
+case ‘tell me a joke’:
+bot.sendMessage({
+to: channelID,
+message: "how do you keep an idiot in suspense?"
+});    
 case ‘fuck you’:
 bot.sendMessage({
 to: channelID,
